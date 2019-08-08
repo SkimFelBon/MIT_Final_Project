6b128f1b3109e0f746dfc71243748d5202a04ad8
SkimFelBon <kleonid6@gmail.com>
2019-08-08 17:49:29 +0300

    end of day

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 # app.py
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
-
+import datetime
 ##import logging
 ##logging.basicConfig(format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
 ##logging.warning('is when this event was logged.')
@@ -14,17 +14,30 @@ from models import Wind_date, Wind_speed
 @app.route("/")
 def index():
     # TODO: index page should provide last 5 min of wind speed
+    # range query from mm:ss to mm:sec
+    # how to specify date??
+    DateStart = datetime.datetime(2019, 8, 6, 14, 48, 1, 7732)
+    DateEnd = datetime.datetime(2019, 8, 6, 14, 56, 12, 726856)
+    # someDate = (50, '2019-08-06 14:56:12.726856')
+    # Probably I need to convert string to DateTimeObject
+    # datetime.datetime(2019, 8, 8, 14, 25, 58, 376145)
+    # (1, '2019-08-06 14:48:01.007732')
+    # ...
+    # (50, '2019-08-06 14:56:12.726856')
+
+    ## qry = DBSession.query(User).filter(User.birthday.between('1985-01-17', '1988-01-17'))
+    qry = Wind_date.query.filter_by(Record_dt >= DateStart).all()
+    # return range of speeds/dates
+    app.logger.warning(f"Dates in Range: {qry}")
     ImageLocation = "static/images/Figure_1.png"
     return render_template("index.html", ImageLocation=ImageLocation)
 
 @app.route("/alldata")
-def deadEnd():
-    # DONE: simple case query db for all data
-    myQuery = Wind_speed.query.all()
-    app.logger.warning(f'Wind speed from db: {myQuery}')
-    # TODO: query db
-    # range query from mm:ss to mm:sec
-    # return range of speeds
+def alldata():
+    # TODO: simple case query db for all data
+    ## myQuery = Wind_speed.query.all()
+    ## app.logger.warning(f'Wind speed from db: {myQuery}')
+
     ImageLocation = "static/images/Figure_1.png"
     return render_template("alldata.html", ImageLocation=ImageLocation)
 
